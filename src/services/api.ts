651d2682911cad81e909d6d7f952b564e9589b25
abdulmoeezhassan <abdulmoeez2708@gmail.com>
2026-07-21 22:41:00 +0500

    fix: prod url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { readAuthToken } from '../utils/authStorage'
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL?.trim() || 'https://ai-recruitment-system-backend-b03gmyqti.vercel.app/'
+const baseUrl = import.meta.env.VITE_API_BASE_URL?.trim() || 'https://github.com/DevWhiz1/talentflow-frontend.git/api/v1'
 export const apiBaseUrl = baseUrl
 
 export const api = axios.create({
